91fd16ebdea228251a2cbd4a7963390fed5ee6f6
Alberto Varesio <jcn9000@gmail.com>
2024-10-12 18:37:26 +0200

    V3 dev version

--- pdf2rmnotebook.sh
+++ pdf2rmnotebook.sh
@@ -1,30 +1,49 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: ./pdf2rmnotebook.sh
-# 
+#
 #         USAGE: ./pdf2rmnotebook.sh [options] file.pdf [...]
-# 
+#
 #   DESCRIPTION: Build multi page reMarkable Notebook from PDF Files
-# 
+#
 #       OPTIONS: See Usage()
 #  REQUIREMENTS: drawj2d, pdfinfo
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Alberto Varesio (JCN), jcn9000@gmail.com
-#  ORGANIZATION: 
+#  ORGANIZATION:
 #       CREATED: 05/26/2021 01:24:45 PM
 #      REVISION: 1.1   : Multi page PDF conversion
 #                1.2.0 : Usage, Verbosity and Version options
 #                1.3.0 : Image inclusion
 #                2.2.0 : option to create reMarkable Notebook .rmn file
+#                3.0.0 : Color management for Pro, rmdoc
 #===============================================================================
+
+#  reMarkable 2: device screen width = 157 mm, height = 209 mm
+#  reMarkable 2: monochrome display, drawj2d will map colours to black, grey or white
+#  reMarkable Pro: device screen width = 179 mm, height = 239 mm. Preview drawj2d -Tscreen -W168 -H239 thales.hcl
+#
+#  reMarkable 2:
+# Embed page 5 of an A4 sized (297mm x 210mm) pdf file, scale to fit the tablet height (297 * 0.7 = 208mm < 209mm), right justified (9 + 210 * 0.7 = 156mm < 157mm).
+#    moveto 9 0
+#    image article.pdf 5 0 0 0.7
+#
+# reMarkable Pro:
+# Embed page 5 of an A4 sized (297mm x 210mm) pdf file, scale to fit the tablet height (297 * 0.8 = 238mm < 239mm), right justified (210*0.8 = 168mm <= 179mm - 11mm).
+#     move10 11 0
+#     image article.pdf 5 0 0 0.8
+#  convert pappagallo.jpg pappagallo.pdf
+#  convert pappagallo.pdf -dither FloydSteinberg -remap colormap.png rM-pappagallo.pdf
+
+# ~/Python-Env/rmc/bin/rmc -t rm  -o GrafanaMail.rm GrafanaMail.md
 
 set -o nounset                              # Treat unset variables as an error
 
-Version=2.2.0
+Version=3.0.0
 
-NAME=$(basename $0 .sh)
+NAME=$( basename ${BASH_SOURCE[0]} .sh )
 TEMP=$(mktemp -d)
 
 # === Functions ===
@@ -37,18 +56,19 @@ Usage:
 Create multi-page reMarkable Notebook file from PDF files
   * Creates .zip files by default for use with rmapi
   * Use -r option to create a reMarkable Notebook .rmn file for use with RCU
+  * Use -R option to create a reMarkable Notebook .rmdoc file for use with USB Link
 
-Options:
   Switches (no arguments required):
     -h    Display this help and exit
     -q    Produce fewer messages to stdout
     -r    Create a reMarkable Notebook .rmn file (default: zip)
+    -R    Create a reMarkable Notebook .rmdoc file
     -v    Produce more messages to stdout
     -V    Display version information and exit
 
   With arguments:
     -n NAME    Set the rmn Notebook Display Name (default: Notebook-<yyyymmdd_hhmm.ss>)
-               Only used with -r option
+               Only used with -r/-R option
     -o FILE    Set the output filename (default: Notebook-<yyyymmdd_hhmm.ss>.zip)
     -s SCALE   Set the scale value (default: 0.75) - 0.75 is a good value for A4/Letter PDFs
 
@@ -62,7 +82,7 @@ Cleanup() {
 }
 
 # === Main ===
-trap Cleanup EXIT
+trap Cleanup EXIT SIGQUIT SIGTERM
 
 QVFLAG='-q'
 VERBOSE=false
@@ -74,10 +94,27 @@ OUTFILE=""
 EXTENSION=".zip"
 DISPLAY_NAME=$DEFAULT_OUTFILE
 RMN=false
+RMDOC=false
+RMC=false
+RMPRO=false
+TARARGS=
 
-while getopts "dhin:qrvs:Vo:" opt
+# Get useful helper commands
+RMCEXE=$(command -v rmc)
+
+while getopts "dhimn:PqrRvs:Vo:" opt
 do
   case $opt in
+    m)
+      # Convert file using RMC
+      if [ -z "$RMCEXE" ]
+      then
+        echo "Warning, no RMC Command found, No conversion possible"
+        continue
+      fi
+      RMC=true
+      EXTENSION=".rmdoc"
+      ;;
     d)
       # Fake some values
       DEBUG=true
@@ -89,6 +126,10 @@ do
     o)
       OUTFILE=$OPTARG
       ;;
+    P)  # Output is for rM Pro
+      RMPRO=true
+      SCALE=.8
+      ;;
     q)
       QVFLAG='-q'
       VERBOSE=false
@@ -97,6 +138,10 @@ do
       RMN=true
       EXTENSION=".rmn"
       TARARGS="cf"
+      ;;
+    R)
+      RMDOC=true
+      EXTENSION=".rmdoc"
       ;;
     h)
       Usage
@@ -159,39 +204,46 @@ NB=${TEMP}/Notebook
 mkdir ${NB}
 
 _page=0
-UUID_N=$(uuidgen)   # UUID for Notebook
+UUID_NB=$(uuidgen)   # UUID for Notebook
 
-mkdir ${NB}/${UUID_N}
-cp ${VARLIB}/UUID.pagedata ${NB}/${UUID_N}.pagedata
-cp ${VARLIB}/UUID_HEAD.content ${NB}/${UUID_N}.content
+mkdir ${NB}/${UUID_NB}
+#cp ${VARLIB}/UUID.pagedata ${NB}/${UUID_NB}.pagedata
+cp ${VARLIB}/UUID_HEAD.content ${NB}/${UUID_NB}.content
 
+# Work on all files
 for _P in "$@"
 do
-
-#  test ${_page} -ne 0 && echo , >> ${NB}/${UUID_N}.content
 
   $VERBOSE && echo Working on file: ${_P}
   test -f "${_P}" || { echo "${_P}: No such file or directory." ; Usage ; exit 1 ; }
 
   FILETYPE=$(file -b --mime-type "${_P}")
+  if [ -z "$FILETYPE" ]
+  then
+    EXT=${_P##*.}     # Guess filetype from extension
+    case $EXT in
+      md)   # Markdown
+        RMC=true
+        FILETYPE='text/markdown'
+        ;;
+    esac
+  fi
 
   case $FILETYPE in
     image/jpeg | image/png)
       _NP=1
       IMAGE=true
       ;;
-
     application/pdf )
-
-      # Get Pages from file, loop over all of them
-      read x _NP <<< $(pdfinfo "${_P}" | grep Pages: )
+      # Get Pages from file, will loop over all of them
+      read x _NP <<< $(pdfinfo "${_P}" | grep -a Pages: )
       ;;
   esac
 
     _PP=1
     while [[ ${_PP} -le ${_NP} ]]
     do
-      test ${_page} -ne 0 && echo , >> ${NB}/${UUID_N}.content
+      test ${_page} -ne 0 && echo , >> ${NB}/${UUID_NB}.content
       cp ${TEMP}/page.hcl ${TEMP}/P_${_page}.hcl
 
       case $FILETYPE in
@@ -200,20 +252,15 @@ do
           echo "image {${_P}} 200 0 0 $SCALE " >> ${TEMP}/P_${_page}.hcl
           ;;
         application/pdf )
-          # Scale image: Usually PDF is A4/Letter, rM is smaller
+          # Scale page: Usually PDF is A4/Letter, rM is smaller
           echo "image {${_P}} ${_PP} 0 0 $SCALE" >> ${TEMP}/P_${_page}.hcl
           ;;
       esac
 
-      UUID_P=$(uuidgen)   # Notebook Pages should be named using the UUID from .content file
-  #  rmapi interface to reMarkable cloud renames pages from UUID to page number while up/downloading
-  #  so we need to use the same convention in naming pages: 0.rm 1.rm ... instead of UUIDs
-  #  which are used internally in the rM (see ~/.local/share/remarkable/xochitl/ )
-  #  It is indeed easier to have page numbers instead of random UUIDs referenced elsewhere
-      drawj2d ${QVFLAG} -T rm -o ${NB}/${UUID_N}/${_page}.rm ${TEMP}/P_${_page}.hcl
-      cp ${VARLIB}/UUID_PAGE-metadata.json ${NB}/${UUID_N}/${_page}-metadata.json
-
-      echo -n \"${UUID_P}\" >> ${NB}/${UUID_N}.content
+      UUID_P=$(uuidgen)   # Notebook Pages are named using the UUID from .content file
+      drawj2d ${QVFLAG} -T rm -o ${NB}/${UUID_NB}/${UUID_P}.rm ${TEMP}/P_${_page}.hcl
+      cp ${VARLIB}/UUID_PAGE-metadata.json ${NB}/${UUID_NB}/${UUID_P}-metadata.json
+      echo -n \"${UUID_P}\" >> ${NB}/${UUID_NB}.content
 
       _PP=$(( $_PP + 1 ))
       _page=$(( _page + 1 ))
@@ -222,46 +269,46 @@ do
 done
 
 
-cat ${VARLIB}/UUID_TAIL.content >> ${NB}/${UUID_N}.content
+cat ${VARLIB}/UUID_TAIL.content >> ${NB}/${UUID_NB}.content
 # sed in macOS need "backup file" argument
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/%PAGENUM%/${_page}/" ${NB}/${UUID_N}.content
+    sed -i '' "s/%PAGENUM%/${_page}/" ${NB}/${UUID_NB}.content
 else
-    sed -i "s/%PAGENUM%/${_page}/" ${NB}/${UUID_N}.content
+    sed -i "s/%PAGENUM%/${_page}/" ${NB}/${UUID_NB}.content
 fi
-#DEBUG  cat ${NB}/${UUID_N}.content
+$DEBUG && { echo "DEBUG: Dump of .content " ; cat ${NB}/${UUID_NB}.content ; }
 
 (
 cd ${NB}
-if [ $RMN = true ]; then
   if [ $QVFLAG = "-v" ]; then
     TARARGS="${TARARGS}v"
   fi
 
 
-  CMOTIME=$(date +%s000) 
+  CMOTIME=$(date +%s000)
   PAGE=1
   TYPE="DocumentType"
   if [ -z "$DISPLAY_NAME" ]; then
     DISPLAY_NAME=$DEFAULT_OUTFILE
   fi
-  
-  cat > "${NB}/${UUID_N}.metadata" <<EOF
+
+  cat > "${NB}/${UUID_NB}.metadata" <<EOF
   {
     "createdTime": ${CMOTIME},
     "lastModified": ${CMOTIME},
     "lastOpened": ${CMOTIME},
     "lastOpenedPage": ${PAGE},
-    "parent": "", 
+    "parent": "",
     "pinned": false,
     "type": "${TYPE}",
     "visibleName": "${DISPLAY_NAME}"
   }
 EOF
 
-  tar $TARARGS ${TEMP}/Notebook$EXTENSION ${UUID_N}.* ${UUID_N}/* 
+if [ $RMN = true ]; then
+  tar ${TARARGS} ${TEMP}/Notebook$EXTENSION ${UUID_NB}.* ${UUID_NB}/*
 else
-  zip ${QVFLAG} ${TEMP}/Notebook$EXTENSION ${UUID_N}.* ${UUID_N}/*
+  zip ${QVFLAG} ${TEMP}/Notebook$EXTENSION ${UUID_NB}.* ${UUID_NB}/*
 fi
 )
 
@@ -270,5 +317,6 @@ cp ${TEMP}/Notebook$EXTENSION ${OUTFILE}$EXTENSION
 
 echo Output written to $OUTFILE$EXTENSION
 
-#DEBUG  find ${TEMP} -ls
+$DEBUG && { echo "DEBUG: contents of $TEMP: " ; find ${TEMP} -ls ; }
 
+# vim: ts=2:sw=2:expandtab
---
+++ var/lib/pdf2rmnotebook/colormap.hcl
@@ -0,0 +1,35 @@
+moveto 40 30
+pen black
+fillcircle 10
+moveto 70 30
+pen blue
+fillcircle 10
+moveto 100 30
+pen cyan
+fillcircle 10
+moveto 130 30
+pen yellow
+fillcircle 10
+moveto 160 30
+pen green
+fillcircle 10
+
+moveto 40 70
+pen lightgrey
+fillcircle 10
+moveto 70 70
+pen magenta
+fillcircle 10
+moveto 100 70
+pen white
+fillcircle 10
+pen black
+circle 10.1
+moveto 130 70
+pen pink
+fillcircle 10
+moveto 160 70
+pen red
+fillcircle 10
+
+
